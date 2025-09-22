867dd881e4abbc54811d9a42977d230d42ec112c
CharlieChiu <Charlie.chiu@saturn.co>
2025-09-22 13:58:52 -0500

    View Automation History -Modify routes.rb
    Add a top level route for automation_histories index in route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
       resources :automation_histories
     end
   end
+  # Add a top-lvel route for automation_histories index
+  resources :automation_histories, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
