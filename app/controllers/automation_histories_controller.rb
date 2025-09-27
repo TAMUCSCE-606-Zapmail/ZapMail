cac9e4987ad468c5f2024dcf877855bf350cab60
CharlieChiu <Charlie.chiu@saturn.co>
2025-09-27 17:32:22 -0500

    View Automation History - Adding basic search funtion in automation_histories_controller
    Add a query function in automation_histories_controller.rb

--- app/controllers/automation_histories_controller.rb
+++ app/controllers/automation_histories_controller.rb
@@ -1,8 +1,11 @@
 class AutomationHistoriesController < ApplicationController
   def index
+    @query = params[:query]
+
     scope = AutomationHistory
               .joins(automation: :strategy)
               .where(strategies: { user_id: current_user.id })
+              .search(@query)   # 🔍 apply search here
               .order(created_at: :desc)
 
     @pagy, @automation_histories = pagy(scope)
