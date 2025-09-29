72268a38679465570288c75fdcd661a3e8d77744
PRADEEPPERIYASAMYTAMU <papradeep11cs@tamu.edu>
2025-09-29 01:35:02 -0500

    feat: Implement JWT authentication helpers in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,55 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # Protect from CSRF attacks, but use null_session for API-style token auth
+  protect_from_forgery with: :null_session
+
+  helper_method :current_user, :logged_in?
+
+  private
+
+  # --- JWT Token Handling ---
+
+  def encode_token(payload)
+    # Set token to expire in 24 hours
+    payload[:exp] = 24.hours.from_now.to_i
+    JWT.encode(payload, JsonWebToken::SECRET_KEY)
+  end
+
+  def decoded_token
+    # Get token from the Authorization header or cookies
+    auth_header = request.headers['Authorization']
+    if auth_header
+      token = auth_header.split(' ').last
+    else
+      token = cookies[:jwt]
+    end
+
+    if token
+      begin
+        JWT.decode(token, JsonWebToken::SECRET_KEY, true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  # --- User Session Management ---
+
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @current_user ||= User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorize
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this page."
+      redirect_to login_url
+    end
+  end
 end
