07778fda0f10ae06c4ad6cf5f49a7d97121d1a03
Yifei Wang <yifeiwang@tamu.edu>
2025-10-04 18:35:32 -0500

    Normalize line endings for Heroku Linux environment

--- config/routes.rb
+++ config/routes.rb
@@ -12,33 +12,6 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'signup', to: 'users#new'
-  resources :users, only: [:create, :show, :edit, :update]
-
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-
-  get 'profile', to: 'users#show'
-  get 'profile/edit', to: 'users#edit'
-  patch 'profile', to: 'users#update'
-
-  get "up" => "rails/health#show", as: :rails_health_check
-end
-
-Rails.application.routes.draw do
-  root "templates#index"
-
-  resources :templates do
-    member do
-      post :preview
-      post :schedule
-    end
-    collection do
-      post :verify_spreadsheet
-      post :preview
-    end
-  end
   resources :automations, only: [:index, :show]
 
   get 'signup', to: 'users#new'
@@ -54,4 +27,3 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
-
