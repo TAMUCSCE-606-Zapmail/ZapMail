9eca51ab00f721f0a1afb6c9790e06ac55a81d09
PRADEEPPERIYASAMYTAMU <papradeep11cs@tamu.edu>
2025-10-04 15:55:00 -0500

    fix: udpate redis config for prod

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,17 +4,18 @@ require 'sidekiq'
 
 redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379/1')
 
-# If the URL uses SSL (rediss://), add ssl_params for Heroku/other SSL Redis
-ssl_params = if redis_url.start_with?('rediss://')
-               { verify_mode: OpenSSL::SSL::VERIFY_NONE } # skip verification for self-signed certs
-             else
-               {}
-             end
-
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url }.merge(ssl_params)
+  config.redis = if redis_url.start_with?('rediss://')
+                   { url: redis_url, ssl: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+                 else
+                   { url: redis_url }
+                 end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url }.merge(ssl_params)
+  config.redis = if redis_url.start_with?('rediss://')
+                   { url: redis_url, ssl: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+                 else
+                   { url: redis_url }
+                 end
 end
