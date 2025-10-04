d39cc1c895c88d0756e9d5c1d4b64099b4fde555
PRADEEPPERIYASAMYTAMU <papradeep11cs@tamu.edu>
2025-10-04 15:57:19 -0500

    fix: udpate redis config for prod

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,21 +1,24 @@
 # config/initializers/sidekiq.rb
 
 require 'sidekiq'
+require 'openssl' # <--- CRITICAL: Make sure this line is present!
 
 redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379/1')
 
+# This logic attempts to disable verification (OpenSSL::SSL::VERIFY_NONE is 0)
+ssl_params = if redis_url.start_with?('rediss://')
+               # NOTE: verify_mode: OpenSSL::SSL::VERIFY_NONE is a security risk.
+               # Only use this if required by your hosting provider.
+               { ssl: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+             else
+               {}
+             end
+
 Sidekiq.configure_server do |config|
-  config.redis = if redis_url.start_with?('rediss://')
-                   { url: redis_url, ssl: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
-                 else
-                   { url: redis_url }
-                 end
+  # Merge the ssl parameters directly into the redis configuration hash
+  config.redis = { url: redis_url }.merge(ssl_params)
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = if redis_url.start_with?('rediss://')
-                   { url: redis_url, ssl: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
-                 else
-                   { url: redis_url }
-                 end
+  config.redis = { url: redis_url }.merge(ssl_params)
 end
