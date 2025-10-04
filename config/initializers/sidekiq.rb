e7bca8ad17859acc5d829ab65843f5c0593662b7
PRADEEPPERIYASAMYTAMU <papradeep11cs@tamu.edu>
2025-10-04 16:09:12 -0500

    fix: udpate redis config for prod

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,33 +2,23 @@
 
 require 'sidekiq'
 require 'openssl'
-require 'redis'
 
 redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379/1')
 
 # Configure Redis connection with SSL bypass for production
 if redis_url.start_with?('rediss://')
-  # Create a custom Redis connection that bypasses SSL verification
+  # For SSL Redis connections, use URL with SSL parameters
   # WARNING: This is a security risk but may be required for some hosting providers
   
-  # Parse the Redis URL to extract components
-  uri = URI.parse(redis_url)
-  
-  # Create Redis client with SSL verification disabled
-  redis_client = Redis.new(
-    host: uri.host,
-    port: uri.port,
-    password: uri.password,
-    db: uri.path&.split('/')&.last&.to_i || 0,
-    ssl: true,
+  redis_config = {
+    url: redis_url,
     ssl_params: {
       verify_mode: OpenSSL::SSL::VERIFY_NONE,
       verify_hostname: false
     }
-  )
-  
-  redis_config = { client: redis_client }
+  }
 else
+  # For non-SSL Redis connections
   redis_config = { url: redis_url }
 end
 
